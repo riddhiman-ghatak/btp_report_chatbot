401f3916fb0b65f2a71625c6ecea9d1a0190e70e
Riddhiman Ghatak <riddhimanghatak@gmail.com>
2025-01-11 01:56:46 +0530

    minor changes

--- image_extract.py
+++ image_extract.py
@@ -2,7 +2,7 @@ import fitz  # PyMuPDF
 import os
 
 def extract_pages_as_png(pdf_path, output_folder):
-    # Ensure the output folder exists
+    
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -11,7 +11,7 @@ def extract_pages_as_png(pdf_path, output_folder):
     for page_num in range(len(pdf_document)):
         page = pdf_document.load_page(page_num)
 
-        zoom = 2  # Zoom factor for higher resolution (e.g., 2x)
+        zoom = 2  
         mat = fitz.Matrix(zoom, zoom)
 
         pix = page.get_pixmap(matrix=mat, alpha=False)
@@ -23,6 +23,6 @@ def extract_pages_as_png(pdf_path, output_folder):
     print(f"All pages extracted and saved to {output_folder}")
 
 if __name__ == "__main__":
-    pdf_path = "test1.pdf"  # Replace with your PDF file path
-    output_folder = "output_images"  # Replace with your desired output folder
+    pdf_path = "test1.pdf"  
+    output_folder = "output_images"  
     extract_pages_as_png(pdf_path, output_folder)
